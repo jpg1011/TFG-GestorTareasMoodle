09c6973bd55cbf75cfbeded841e55902950c3bd5
Javier <jpg1011@alu.ubu.es>
2025-02-18 17:29:42 +0100

    #4 Se implementa sistema de filtrado

--- app/app/lib/screens/gantt_chart_screen.dart
+++ app/app/lib/screens/gantt_chart_screen.dart
@@ -1,3 +1,6 @@
+import 'package:app/models/assign.dart';
+import 'package:app/models/courses.dart';
+import 'package:app/models/quiz.dart';
 import 'package:flutter/material.dart';
 import 'package:gantt_chart/gantt_chart.dart';
 import 'package:app/models/user_model.dart';
@@ -12,34 +15,167 @@ class GanttChartScreen extends StatefulWidget {
 }
 
 class _GanttChartScreenState extends State<GanttChartScreen> {
-  List<GanttAbsoluteEvent> getEvents() {
-    var events = <GanttAbsoluteEvent>[];
-    for (var course in widget.user.userCourses!) {
+  List<dynamic> _events = [];
+  bool selectTask = false;
+  bool selectQuiz = false;
+
+  List<dynamic> getEvents(List<Courses> selectedCourses) {
+    List<dynamic> events = [];
+
+    final coursesToShow = selectedCourses.isEmpty
+        ? widget.user.userCourses!
+        : widget.user.userCourses!
+            .where((course) => selectedCourses.contains(course));
+
+    for (var course in coursesToShow) {
       if (course.assignments != null) {
         for (var event in course.assignments!) {
-          if(event.duedate*1000 >= DateTime.now().millisecondsSinceEpoch){
-            events.add(GanttAbsoluteEvent(
-              startDate: DateTime.fromMillisecondsSinceEpoch(event.allowsubmissionsfromdate*1000),
-              endDate: DateTime.fromMillisecondsSinceEpoch(event.duedate*1000),
-              displayName: event.name
-            )
-          );
+          if (event.duedate * 1000 >= DateTime.now().millisecondsSinceEpoch) {
+            events.add(event);
           }
         }
+      }
 
+      if (course.quizzes != null) {
         for (var event in course.quizzes!) {
-          if(event.timeclose*1000 >= DateTime.now().millisecondsSinceEpoch){
-            events.add(GanttAbsoluteEvent(
-              startDate: DateTime.fromMillisecondsSinceEpoch(event.timeopen*1000),
-              endDate: DateTime.fromMillisecondsSinceEpoch(event.timeclose*1000),
-              displayName: event.name
-            )
-          );
+          if (event.timeclose * 1000 >= DateTime.now().millisecondsSinceEpoch) {
+            events.add(event);
           }
         }
       }
     }
+
     return events;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _events = getEvents([]);
+    selectTask = true;
+    selectQuiz = true;
+  }
+
+  Future<void> _showFilterMenu(BuildContext context) async {
+    final List<dynamic> types = [Assign, Quiz];
+
+    final List<Courses>? selectedCourses =
+        await showModalBottomSheet<List<Courses>>(
+            context: context,
+            builder: (BuildContext context) {
+              List<Courses> coursesToFilter = [];
+              return StatefulBuilder(builder: (BuildContext context, setState) {
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        'Filtrar diagrama',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      Wrap(
+                        alignment: WrapAlignment.spaceEvenly,
+                        spacing: 5.0,
+                        children: [
+                          ...widget.user.userCourses!.map((course) {
+                            return FilterChip(
+                                label: Text(course.shortname),
+                                selected: coursesToFilter.contains(course),
+                                onSelected: (bool selected) {
+                                  setState(() {
+                                    if (selected) {
+                                      coursesToFilter.add(course);
+                                    } else {
+                                      coursesToFilter.remove(course);
+                                    }
+                                  });
+                                });
+                          })
+                        ],
+                      ),
+                      const SizedBox(height: 10.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          FilterChip(
+                            label: const Text('Tareas'),
+                            selected: selectTask,
+                            onSelected: (bool value) {
+                              setState(
+                                () {
+                                  if (value) {
+                                    selectTask = value;
+                                  } else {
+                                    selectTask = false;
+                                  }
+                                },
+                              );
+                            },
+                          ),
+                          FilterChip(
+                            label: const Text('Cuestionarios'),
+                            selected: selectQuiz,
+                            onSelected: (bool value) {
+                              setState(
+                                () {
+                                  if (value) {
+                                    selectQuiz = value;
+                                  } else {
+                                    selectQuiz = false;
+                                  }
+                                },
+                              );
+                            },
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text('Cancelar')),
+                          ElevatedButton(
+                              onPressed: () =>
+                                  Navigator.pop(context, coursesToFilter),
+                              child: const Text('Aceptar'))
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              });
+            });
+
+    if (selectedCourses != null) {
+      setState(() {
+        _events = getEvents(selectedCourses);
+      });
+    }
+  }
+
+  GanttAbsoluteEvent getGanttEvent(dynamic event) {
+    if (event.runtimeType == Assign && selectTask) {
+      return GanttAbsoluteEvent(
+        displayName: event.name,
+        startDate: DateTime.fromMillisecondsSinceEpoch(
+            event.allowsubmissionsfromdate * 1000),
+        endDate: DateTime.fromMillisecondsSinceEpoch(event.duedate * 1000),
+      );
+    } else if (event.runtimeType == Quiz && selectQuiz) {
+      return GanttAbsoluteEvent(
+          displayName: event.name,
+          startDate: DateTime.fromMillisecondsSinceEpoch(event.timeopen * 1000),
+          endDate: DateTime.fromMillisecondsSinceEpoch(event.timeclose * 1000)
+      );
+    }
+    return GanttAbsoluteEvent(
+        displayName: 'No hay eventos',
+        startDate: DateTime.now(),
+        endDate: DateTime.now()
+    );
   }
 
   @override
@@ -50,11 +186,37 @@ class _GanttChartScreenState extends State<GanttChartScreen> {
         title: const Text("Diagrama Gantt"),
         backgroundColor: Theme.of(context).primaryColor,
       ),
-      body: GanttChartView(
-        startOfTheWeek: WeekDay.monday,
-        weekEnds: {WeekDay.saturday, WeekDay.sunday},
-        startDate: DateTime(2025, 1, 30),
-        events: getEvents()
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        _showFilterMenu(context);
+                      },
+                      style: ButtonStyle(
+                          backgroundColor:
+                              WidgetStatePropertyAll(Colors.cyan[100])),
+                      child: const Icon(Icons.filter_list, color: Colors.white))
+                ],
+              ),
+            ),
+            GanttChartView(
+              startOfTheWeek: WeekDay.monday,
+              weekEnds: {WeekDay.saturday, WeekDay.sunday},
+              startDate: DateTime(2025, 1, 30),
+              events: _events.isEmpty
+                  ? []
+                  : _events.map((event) {
+                      return getGanttEvent(event);
+                    }).toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
