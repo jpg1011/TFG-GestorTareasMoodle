e5ee728991b7407a7f7ab9b3dd1b7a6c228bf26b
Javier <jpg1011@alu.ubu.es>
2025-03-19 11:38:05 +0100

    Se cambia el tema principal de la app

--- app/app/lib/app.dart
+++ app/app/lib/app.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Gestor Tareas UBU',
-      theme: ThemeData(primaryColor: const Color(0xFFF4F4F4)),
+      theme: ThemeData(primaryColor: const Color.fromARGB(255, 255, 255, 255)),
       initialRoute: '/login',
       routes: {'/login': (context) => const LoginScreen()},
     );
