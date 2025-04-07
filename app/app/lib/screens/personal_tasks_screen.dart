de8a7649f53b931295e8b1d82be7fce01afb5eb3
Javier <jpg1011@alu.ubu.es>
2025-04-07 11:38:34 +0200

    #17 Se añade pantalla con formulario de creación de tareas personales

---
+++ app/app/lib/screens/personal_tasks_screen.dart
@@ -0,0 +1,267 @@
+import 'dart:collection';
+
+import 'package:app/models/courses.dart';
+import 'package:app/models/user_model.dart';
+import 'package:app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class PersonalTasksScreen extends StatefulWidget {
+  final UserModel user;
+
+  const PersonalTasksScreen({super.key, required this.user});
+
+  @override
+  State<PersonalTasksScreen> createState() => _PersonalTasksScreenState();
+}
+
+typedef MenuEntry = DropdownMenuEntry<String>;
+
+class _PersonalTasksScreenState extends State<PersonalTasksScreen> {
+  TextEditingController _taskname = TextEditingController();
+  TextEditingController _taskdescription = TextEditingController();
+  TextEditingController _taskCourse = TextEditingController();
+  String? taskCourse;
+  DateTime? taskStartDate;
+  DateTime? taskEndDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) =>
+                                HomeScreen(user: widget.user)));
+                      },
+                      icon: const Icon(Icons.arrow_back, color: Colors.black)),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: FloatingActionButton(
+                      backgroundColor: Colors.blue,
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(45)),
+                      onPressed: () {
+                        addTaskDialog();
+                      },
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      )),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  addTaskDialog() {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(builder: (context, setDialogState) {
+          return AlertDialog(
+            backgroundColor: Theme.of(context).primaryColor,
+            title: const Text('Nueva tarea',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                textAlign: TextAlign.center),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: _taskname,
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                      hintText: 'Nombre de la tarea',
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 5),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              const BorderSide(color: Colors.blue, width: 2)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _taskname,
+                  cursorColor: Colors.black,
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                      hintText: 'Descripción de la tarea',
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 5),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              const BorderSide(color: Colors.blue, width: 2)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+                const SizedBox(height: 10),
+                DropdownMenu(
+                  controller: _taskCourse,
+                  enableSearch: false,
+                  hintText: 'Seleccione curso',
+                  menuStyle: MenuStyle(
+                    backgroundColor:
+                        WidgetStatePropertyAll(Theme.of(context).primaryColor),
+                  ),
+                  inputDecorationTheme: InputDecorationTheme(
+                      outlineBorder: const BorderSide(color: Colors.black),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              const BorderSide(color: Colors.blue, width: 2)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  onSelected: (value) {
+                    setState(() {
+                      taskCourse = value;
+                    });
+                    setDialogState(() {});
+                  },
+                  dropdownMenuEntries: UnmodifiableListView<MenuEntry>(
+                    widget.user.userCourses!.map<MenuEntry>((Courses course) =>
+                        MenuEntry(
+                            value: course.fullname, label: course.fullname)),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10)),
+                        height: 45,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Expanded(
+                                child: Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Text(
+                                      taskStartDate != null
+                                          ? DateFormat('dd/MM/y')
+                                              .format(taskStartDate!)
+                                          : 'dd/mm/aaaa',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black.withOpacity(0.5)),
+                                    ))),
+                            const VerticalDivider(),
+                            IconButton(
+                                onPressed: () async {
+                                  DateTime? startDate = await showDatePicker(
+                                      context: context,
+                                      builder: (context, child) {
+                                          return Theme(
+                                            data: ThemeData(useMaterial3: false), 
+                                            child: child ?? const SizedBox()
+                                          );
+                                        },
+                                      firstDate: DateTime.now(),
+                                      lastDate: taskEndDate ?? DateTime(2100),
+                                      locale: const Locale('es', 'ES'),
+                                      helpText: 'Fecha inicial',
+                                      confirmText: 'Aceptar',
+                                      cancelText: 'Cancelar');
+                                  setState(() {
+                                    taskStartDate = startDate;
+                                  });
+                                  setDialogState(() {});
+                                },
+                                icon: const Icon(Icons.event_available))
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10)),
+                        height: 45,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Expanded(
+                                child: Align(
+                                    alignment: Alignment.centerRight,
+                                    child: Text(
+                                      taskEndDate != null
+                                          ? DateFormat('dd/MM/y')
+                                              .format(taskEndDate!)
+                                          : 'dd/mm/aaaa',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black.withOpacity(0.5)),
+                                    ))),
+                            const VerticalDivider(),
+                            Align(
+                              alignment: Alignment.center,
+                              child: IconButton(
+                                  onPressed: () async {
+                                    DateTime? endDate = await showDatePicker(
+                                        context: context,
+                                        builder: (context, child) {
+                                          return Theme(
+                                            data: ThemeData(useMaterial3: false), 
+                                            child: child ?? const SizedBox()
+                                          );
+                                        },
+                                        firstDate:
+                                            taskStartDate ?? DateTime.now(),
+                                        lastDate: DateTime(2100),
+                                        locale: const Locale('es', 'ES'),
+                                        helpText: 'Fecha final',
+                                        confirmText: 'Aceptar',
+                                        cancelText: 'Cancelar');
+                                    setState(() {
+                                      taskEndDate = endDate;
+                                    });
+                                    setDialogState(() {});
+                                  },
+                                  icon: const Icon(Icons.event_busy)),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10),
+                TextButton(
+                    style: const ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.blue),
+                    ),
+                    onPressed: () {},
+                    child: const Text('Crear tarea',
+                        style: TextStyle(color: Colors.white)))
+              ],
+            ),
+          );
+        });
+      },
+    );
+  }
+}
