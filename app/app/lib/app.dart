ba4de72290365fa127446b0083e1b0b039e38e2d
Javier <jpg1011@alu.ubu.es>
2025-04-07 11:35:09 +0200

    Se adapta la app para soportar idioma español

--- app/app/lib/app.dart
+++ app/app/lib/app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:app/screens/login_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate
+      ],
+      locale: const Locale('es','ES'),
       debugShowCheckedModeBanner: false,
       title: 'Gestor Tareas UBU',
       theme: ThemeData(primaryColor: const Color.fromARGB(255, 255, 255, 255)),
