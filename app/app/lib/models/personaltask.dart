10e1125a499f13bfbd25b7fadbef943fcc0664d2
Javier <jpg1011@alu.ubu.es>
2025-04-07 11:37:56 +0200

    #17 Se añade modelo de las tareas personales

---
+++ app/app/lib/models/personaltask.dart
@@ -0,0 +1,9 @@
+class PersonalTask {
+  String? name;
+  String? course;
+  DateTime? startdate;
+  DateTime? enddate;
+  String? description;
+
+  PersonalTask({required this.name, required this.course, required this.startdate, required this.enddate, required this.description});
+}
