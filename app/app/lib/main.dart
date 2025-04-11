d8958466a4c566a2039a4e775d9f3ec7afc051ee
Javier <jpg1011@alu.ubu.es>
2025-04-11 19:12:14 +0200

    #17 Se adapta main para usar con Supabase

--- app/app/lib/main.dart
+++ app/app/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+      url: dotenv.env['SUPABASE_URL'] ?? '',
+      anonKey: dotenv.env['SUPABASE_KEY'] ?? ''
+  );
   runApp(MyApp());
 }
