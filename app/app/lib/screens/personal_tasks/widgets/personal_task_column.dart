aaa4bf7f101599ea2318651a333f4963904ad290
Javier <jpg1011@alu.ubu.es>
2025-04-27 13:57:59 +0200

    #20 Widget para agrupar tareas en columnas

---
+++ app/app/lib/screens/personal_tasks/widgets/personal_task_column.dart
@@ -0,0 +1,38 @@
+import 'package:app/models/personaltask.dart';
+import 'package:app/screens/personal_tasks/widgets/personal_task_card.dart';
+import 'package:flutter/material.dart';
+
+class PersonalTaskColumn extends StatelessWidget {
+  final List<PersonalTask> tasks;
+  final String columnName;
+  const PersonalTaskColumn(this.tasks, {super.key, required this.columnName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      margin: const EdgeInsets.all(4.0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          border: Border.all(color: Colors.black)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(columnName, style: const TextStyle(fontWeight: FontWeight.bold)),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: tasks.map((task) => PersonalTaskCard(task)).toList(),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
