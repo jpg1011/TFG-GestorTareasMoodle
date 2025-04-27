3968479180416ec2c788ed34d78997105d46aed0
Javier <jpg1011@alu.ubu.es>
2025-04-27 13:56:43 +0200

    Adaptación de la ruta para pantalla de tareas personales

--- app/app/lib/screens/home_screen.dart
+++ app/app/lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:app/screens/personal_tasks_screen.dart';
+import 'package:app/screens/personal_tasks/personal_tasks_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:app/screens/gantt_chart_screen.dart';
 import 'package:app/models/user_model.dart';
