1b11e4fa04057d69107a69311a36cf6239d1522c
Javier <jpg1011@alu.ubu.es>
2025-05-26 18:28:48 +0200

    Modificación del main

--- app/app/lib/main.dart
+++ app/app/lib/main.dart
@@ -2,15 +2,16 @@ import 'package:app/config/theme/app_theme.dart';
 import 'package:app/presentation/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
+  await initializeDateFormatting('es', null);
   await Supabase.initialize(
       url: dotenv.env['SUPABASE_URL'] ?? '',
-      anonKey: dotenv.env['SUPABASE_KEY'] ?? ''
-  );
+      anonKey: dotenv.env['SUPABASE_KEY'] ?? '');
   runApp(MyApp());
 }
 
@@ -18,10 +19,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate
-      ],
-      locale: const Locale('es','ES'),
+      localizationsDelegates: const [GlobalMaterialLocalizations.delegate],
+      locale: const Locale('es', 'ES'),
       debugShowCheckedModeBanner: false,
       title: 'Gestor Tareas UBU',
       theme: AppTheme().theme(),
