f85fb47b0dbb657acad6d737acd399423ad1c178
Javier <jpg1011@alu.ubu.es>
2025-05-26 20:10:16 +0200

    Se añaden nuevos atributos al modelo PersonalTask

--- app/app/lib/models/personaltask.dart
+++ app/app/lib/models/personaltask.dart
@@ -1,6 +1,7 @@
 enum TaskPriority { alta, media, baja }
 
 class PersonalTask {
+  int? id;
   int userid;
   String moodleid;
   String name;
@@ -8,16 +9,20 @@ class PersonalTask {
   String course;
   DateTime date;
   bool done;
+  DateTime? finishedat;
   TaskPriority priority;
 
   PersonalTask(
-      {required this.userid,
+      {
+      this.id,
+      required this.userid,
       required this.moodleid,
       required this.name,
       required this.description,
       required this.course,
       required this.date,
       this.done = false,
+      this.finishedat,
       required this.priority});
 
   Map<String, dynamic> toMap() {
@@ -29,18 +34,25 @@ class PersonalTask {
       'course': course,
       'date': date.toLocal().toIso8601String(),
       'priority': priority.name,
+      'finishedat': finishedat?.toIso8601String(),
       'done': done
     };
   }
 
   factory PersonalTask.fromMap(Map<String, dynamic> map) {
     return PersonalTask(
+        id: map['id'],
         userid: map['user_id'] as int,
         moodleid: map['moodle_id'].toString(),
         name: map['name'] as String,
         description: map['description'] as String,
         course: map['course'] as String,
         date: DateTime.parse(map['date']),
-        priority: TaskPriority.values.byName(map['priority']));
+        priority: TaskPriority.values.byName(map['priority']),
+        finishedat: map['finishedat'] != null
+            ? DateTime.parse(map['finishedat']).toLocal()
+            : null,
+        done: map['done']
+    );
   }
 }
