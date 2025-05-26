0083af07e604291cd3ff6cb3299ad72ff8e95acc
Javier <jpg1011@alu.ubu.es>
2025-05-26 20:11:11 +0200

    Se añaden metodos update y delete

--- app/app/lib/models/databases/personaltask_database.dart
+++ app/app/lib/models/databases/personaltask_database.dart
@@ -23,4 +23,25 @@ class PersonalTaskDatabase {
         .eq('moodle_id', moodleid);
     return data;
   }
+
+  //Update
+  Future updateTask(PersonalTask personalTask) async {
+    await database.update({
+      'id': personalTask.id!,
+      'user_id': personalTask.userid,
+      'moodle_id': personalTask.moodleid,
+      'name': personalTask.name,
+      'description': personalTask.description,
+      'course': personalTask.course,
+      'date': personalTask.date.toLocal().toIso8601String(),
+      'finishedat': personalTask.finishedat?.toLocal().toIso8601String(), 
+      'done': personalTask.done,
+      'priority': personalTask.priority.name
+      }).eq('id', personalTask.id!).select();
+  }
+
+  //Delete
+  Future deleteTask(PersonalTask task) async{
+    await database.delete().eq('id', task.id!);
+  }
 }
